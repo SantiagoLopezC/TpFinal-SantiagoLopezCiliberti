af8229b745b02154c3636ab504ba5d8c9ba7a4fb
Santiago López Ciliberti <88456914+SantiagoLopezC@users.noreply.github.com>
2023-02-22 12:28:47 -0300

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+var Id="";
+var Sección="";
+function CambiarColorBoton(id){
+    var Contenido =document.getElementById("Boton"+id).style.backgroundColor="yellow";
+}
+function VolverColorAnterior(id){
+    var Contenido =document.getElementById("Boton"+id).style.backgroundColor="";
+}
+
